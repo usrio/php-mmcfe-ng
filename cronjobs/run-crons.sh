5447f8fe92ab549c4900cc3ac6acb9b84a6db472
anc <anc@usr.io>
2013-10-28 11:43:42 -0700

    cron

--- cronjobs/run-crons.sh
+++ cronjobs/run-crons.sh
@@ -10,10 +10,11 @@
 PHP_BIN=$( which php )
 
 # List of cruns to execute
-CRONS="findblock.php proportional_payout.php pplns_payout.php pps_payout.php blockupdate.php manual_payout.php auto_payout.php notifications.php statistics.php archive_cleanup.php"
+CRONS="findblock.php proportional_payout.php pplns_payout.php pps_payout.php blockupdate.php manual_payout.php auto_payout.php tickerupdate.php notifications.php statistics.php archive_cleanup.php"
+#CRONS="findblock.php proportional_payout.php pps_payout.php blockupdate.php tickerupdate.php notifications.php statistics.php archive_cleanup.php"
 
 # Output additional runtime information
-VERBOSE="1"
+VERBOSE="0"
 
 # Base path for PIDFILE, (full path).
 BASEPATH="/tmp"
